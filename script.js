3d3152d1a11c75b30d523401d5b07274244221e7
Paulius Juodsnukis <paulius.juodsnukis@gmail.com>
2021-11-24 02:53:31 +0200

    Add tab functionality.

--- script.js
+++ script.js
@@ -1,1 +1,40 @@
-console.log('Hello console')
+function deactivateAll() {
+    let allContent = document.getElementsByClassName('content')
+    for (let i = 0; i <allContent.length; i++){
+        allContent[i].style.display = 'none';
+    }
+}
+
+function activate(name) {
+    let clockType = document.getElementById(name)
+    clockType.style.display = 'block'
+}
+
+const clockBtn = document.getElementById('clock-btn');
+const timerBtn = document.getElementById('timer-btn');
+const stopwatchBtn = document.getElementById('stopwatch-btn');
+const pomodoroBtn = document.getElementById('pomodoro-btn');
+clockBtn.addEventListener('click',
+    function() {
+        deactivateAll();
+        activate('clock');
+    }
+);
+timerBtn.addEventListener('click',
+    function() {
+        deactivateAll();
+        activate('timer');
+    }
+);
+stopwatchBtn.addEventListener('click',
+    function() {
+        deactivateAll();
+        activate('stopwatch');
+    }
+);
+pomodoroBtn.addEventListener('click',
+    function() {
+        deactivateAll();
+        activate('pomodoro');
+    }
+);
